c6d8b2473fafe55d69a0dfe8cc37fae1bdc717cb
Giulia Cecci <cecci.giulia@franchettisalviani.net>
2026-04-24 10:10:50 +0200

    Initial commit

---
+++ src/ceccimassimitaas/model/Tombolate.java
@@ -0,0 +1,14 @@
+package ceccimassimitaas.model;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author cecci.giulia
+ */
+public class Tombolate {
+    
+}
